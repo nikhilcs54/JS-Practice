bf61e6470771558b3794ebafb327d7360d7660f0
nikhilcs54 <nikhilkumar18101998@gmail.com>
2023-08-13 12:25:13 +0530

    Second Commit
    String updated

---
+++ string.js
@@ -0,0 +1,12 @@
+const text="Happy Frontend";
+
+text.toLowerCase();
+text.toUpperCase();
+text.length;
+text.charAt(2);
+text.includes("py");
+text.concat("Hello");
+text.slice(0,5);
+text.split("");
+text.replace("Frontend","Coding");
+" Nikhil Kumar   ".trim();
